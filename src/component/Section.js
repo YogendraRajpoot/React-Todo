efb8fb5b06b8ccaa849ace904cb788758129b4c8
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-02-08 01:10:00 +0530

    first commit

--- src/component/Section.js
+++ src/component/Section.js
@@ -1,47 +1,18 @@
 import "./Section.css";
-import style from "./strick.css";
 import React, { useState } from "react";
+import { Leftbox } from "./Leftbox.js";
+import { Li } from "./Li";
 
 export const Section = () => {
-  const [value, setvalue] = useState("");
   const [todolist, settodolist] = useState([]);
-  const [isStricked, setIsStricked] = useState(false);
   return (
     <div className="container">
-      <div className="left-box">
-        <p>Todo-List</p>
-        <input
-          placeholder="Make List"
-          value={value}
-          onChange={(element) => setvalue(element.currentTarget.value)}
-        ></input>
-        <button
-          onClick={() => {
-            settodolist([...todolist, value]);
-            setvalue("");
-          }}
-        >
-          ADD
-        </button>
-      </div>
+      <Leftbox todolist={todolist} settodolist={settodolist} />
+      
       <div className="right-box">
         <p>Display-List</p>
         <ul>
-          {todolist.map((todo, value) => {
-            return (
-              <li>
-                <input
-                  type="checkbox"
-                  onChange={() => {
-                    setIsStricked(!isStricked);
-                  }}
-                />
-                <span className={isStricked ? style.strick : style.normal}>
-                  {todo}
-                </span>
-              </li>
-            );
-          })}
+          {todolist.map((todo, value) => <Li value={todo}/>)}
         </ul>
       </div>
     </div>
